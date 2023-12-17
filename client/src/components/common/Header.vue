96befefbe5206b5880de2af8724cf195eac9fc29
Aleksandr <ZeroDay9898@gmail.com>
2023-12-17 23:06:44 +0300

    Basic page navigation

---
+++ client/src/components/common/Header.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+import {RouterLink} from "vue-router";
+</script>
+
+<template>
+  <header class="nav-header">
+    <div class="nav-header__wrapper">
+      <div class="nav-header-block-logo">
+        <span>FD</span>
+      </div>
+      <nav class="nav-items">
+        <div class="nav-item">
+          <div class="nav-item__wrapper">
+            <RouterLink class="nav-item__link" :to="{ name: 'main' }" route="git-repositories">Main</RouterLink>
+          </div>
+        </div>
+        <div class="nav-item">
+          <div class="nav-item__wrapper">
+            <RouterLink class="nav-item__link" :to="{ name: 'git-repositories' }">Repositories</RouterLink>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.nav-header {
+  width: 100%;
+  height: 40px;
+}
+
+.nav-header__wrapper {
+  display: flex;
+  height: 100%;
+  border-bottom: 1px solid var(--color-border);
+}
+
+.nav-header-block-logo {
+  width: 54px;
+}
+
+.nav-header-block-logo > span {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.nav-items {
+  width: 100%;
+  display: flex;
+  align-content: center;
+}
+
+.nav-item {
+  border-left: 1px solid var(--color-border);
+  border-right: 1px solid var(--color-border);
+}
+
+.nav-item__wrapper {
+  padding: 0 4px;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  height: 100%;
+}
+
+.nav-item__link {
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  padding: 16px;
+  color: var(--color-text);
+}
+
+.nav-item__link.router-link-active {
+  color: var(--color-secondary);
+}
+</style>
---
+++ client/src/views/RepositoriesView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    Git repositories
+  </div>
+</template>
+
+<style scoped>
+
+</style>
