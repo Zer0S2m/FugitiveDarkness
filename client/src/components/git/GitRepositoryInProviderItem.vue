1765b750f6ede4e83579d9d9bf696f312338a29e
Aleksandr <ZeroDay9898@gmail.com>
2024-01-31 20:18:11 +0300

    Display information
    Display information about repository capabilities from the git provider

--- client/src/components/git/GitRepositoryInProviderItem.vue
+++ client/src/components/git/GitRepositoryInProviderItem.vue
@@ -1,6 +1,6 @@
 <template>
   <ContainerCardItem>
-    <div>
+    <div class="wrapper">
       <h4 class="git-item__title">
         <a
           :href="item.link"
@@ -8,18 +8,44 @@
           >{{ item.project }}</a
         >
       </h4>
+      <div
+        class="tools"
+        v-if="isLoadRepo"
+      >
+        <CheckIcon class="check" />
+      </div>
     </div>
   </ContainerCardItem>
 </template>
 
 <script setup lang="ts">
+import CheckIcon from '@/assets/check-icon.svg';
 import ContainerCardItem from '@/components/common/ContainerCardItem.vue';
 import type { IGitRepositoryInProvider } from '@/types/gitProvider';
+import { computed } from 'vue';
+import { useGitRepositoryState } from '@/stores/useGitRepositoryState';
 
-defineProps<{ item: IGitRepositoryInProvider }>();
+const props = defineProps<{ item: IGitRepositoryInProvider }>();
+
+const useGitRepositoryStore = useGitRepositoryState();
+
+const isLoadRepo = computed(() => {
+  return useGitRepositoryStore.checkExistsRepoBy_Host_Group_Project(
+    props.item.host,
+    props.item.group,
+    props.item.project
+  );
+});
 </script>
 
 <style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+}
+
 .git-item__title {
   font-size: 18px;
   font-weight: 600;
@@ -27,5 +53,9 @@ defineProps<{ item: IGitRepositoryInProvider }>();
 
 .git-item__title > a {
   color: var(--vt-c-black);
+}
+
+.tools > .check {
+  fill: var(--color-secondary);
 }
 </style>
